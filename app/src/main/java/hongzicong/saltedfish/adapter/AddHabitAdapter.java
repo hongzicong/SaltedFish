20ad7d618ef2698ce133a55ddb0cf816d0d221a3
Dv00 <973138379@qq.com>
2018-01-04 17:49:58 +0800

    9.加入日期dialog

--- app/src/main/java/hongzicong/saltedfish/adapter/AddHabitAdapter.java
+++ app/src/main/java/hongzicong/saltedfish/adapter/AddHabitAdapter.java
@@ -1,13 +1,21 @@
 package hongzicong.saltedfish.adapter;
 
+import android.accessibilityservice.AccessibilityService;
 import android.app.Activity;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.ViewHolder;
 
 import hongzicong.saltedfish.R;
+import hongzicong.saltedfish.utils.UIUtils;
+import hongzicong.saltedfish.viewholder.AddDateViewHolder;
 import hongzicong.saltedfish.viewholder.AddTextViewHolder;
 
 /**
@@ -18,6 +26,7 @@ public class AddHabitAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     private AddTextViewHolder mAddNameViewHolder;
     private AddTextViewHolder mAddDetailViewHolder;
+    private AddDateViewHolder mAddDateViewHolder;
 
     private Context mContext;
     private final int addRowCount=4;
@@ -44,7 +53,24 @@ public class AddHabitAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
             return mAddDetailViewHolder;
         }
         else if(viewType==2){
-            return null;
+            View itemView=layoutInflater.inflate(R.layout.item_date,parent,false);
+            if(mAddDateViewHolder==null){
+                mAddDateViewHolder=new AddDateViewHolder(layoutInflater,parent,"在什么时间结束");
+                mAddDateViewHolder.setOnChangeDateListener(new AddDateViewHolder.OnChangeDateListener() {
+                    @Override
+                    public void showDateDialog() {
+                        DialogPlus dialogPlus=DialogPlus.newDialog(mContext)
+                                .setContentHolder(new ViewHolder(R.layout.dialog_date))
+                                .setCancelable(true)
+                                .setGravity(Gravity.CENTER)
+                                .create();
+                        dialogPlus.show();
+                        InputMethodManager inputMethodManager = (InputMethodManager) mContext.getSystemService(Context.INPUT_METHOD_SERVICE);
+                        inputMethodManager.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+                    }
+                });
+            }
+            return mAddDateViewHolder;
         }
         else if(viewType==3){
             return null;
@@ -59,7 +85,7 @@ public class AddHabitAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolde
 
     @Override
     public int getItemCount() {
-        return 2;
+        return 3;
     }
 
     @Override
