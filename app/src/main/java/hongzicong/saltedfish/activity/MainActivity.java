c8e8cf08615f440ef6c1814043cbb90078371acd
hongzicong <973138379@qq.com>
2018-05-18 13:48:37 +0800

    fix some bug

--- app/src/main/java/hongzicong/saltedfish/activity/MainActivity.java
+++ app/src/main/java/hongzicong/saltedfish/activity/MainActivity.java
@@ -32,9 +32,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
-        localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
-
         ButterKnife.bind(this);
 
         initBottomNavigationBar();
