c99ec81cf472ce1ef0ed99283ff1c07206be92c0
hongzicong <973138379@qq.com>
2018-05-20 21:11:39 +0800

    24. 完善about页面

--- app/src/main/java/hongzicong/saltedfish/activity/AboutActivity.java
+++ app/src/main/java/hongzicong/saltedfish/activity/AboutActivity.java
@@ -4,13 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import hongzicong.saltedfish.R;
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutActivity extends AppCompatActivity {
+
+    @BindView(R.id.relative_layout)
+    RelativeLayout relativeLayout;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
@@ -23,6 +30,19 @@ public class AboutActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         initToolbar();
+
+        View aboutPage = new AboutPage(this)
+                .isRTL(false)
+                .setImage(R.drawable.salted_fish_2)
+                .setDescription("快乐咸鱼每一天")
+                .addItem(new Element().setTitle("Version 1.0"))
+                .addGroup("与我联系")
+                .addEmail("hongzc@mail2.sysu.edu.cn")
+                .addWebsite("http://hongzicong.github.io")
+                .addGitHub("hongzicong")
+                .create();
+
+        relativeLayout.addView(aboutPage);
     }
 
     private void initToolbar(){
